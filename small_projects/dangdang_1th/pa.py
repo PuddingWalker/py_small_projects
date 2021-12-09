47b37cb819160330342a8ac37e56fbd6e1f4d3c3
乌守轲 <wushouke@hummergames.com>
2021-12-09 19:51:40 +0800

    almost finish a dangdang-spider

--- small_projects/dangdang_1th/pa.py
+++ small_projects/dangdang_1th/pa.py
@@ -16,10 +16,7 @@ class dangdang(object):
     def __init__(self):
         self.url = 'http://bang.dangdang.com/books/bestsellers'
         ua = UserAgent(verify_ssl=False)
-        for i in range(1, 100):
-            self.headers = {
-                'User-Agent': ua.random
-            }
+        self.headers = {'User-Agent': ua.random}
 
     def get_html(self, url):
         response = requests.get(url, headers=self.headers)
@@ -28,19 +25,37 @@ class dangdang(object):
 
     def parse_html(self, html):
         target = etree.HTML(html)
-        titles = target.xpath('//p[@class="name"]/a/@title')
-        prices = target.xpath('//p[@class="price"]/span/text()')
-        links = target.xpath('//p[@class="name"]/a/@href')
+        pics = target.xpath('//li/div[@class="pic"]/a/img/@title')
+        names = target.xpath('//li/div[@class="name"]/a/@title')
+        stars = target.xpath(
+            '//li/div[@class="star"]/span[@class="tuijian"]/text()')
+        publisher_infos_foreign = target.xpath(
+            '//li/div[@class="publisher_info"][1]/a[1]/text()')
+        publisher_infos_mainland = target.xpath(
+            '//li/div[@class="publisher_info"][2]/a/text()')
+
+        price_n_s = target.xpath(
+            '//li/div[@class="price"]/p[1]/span[@class="price_n"]/text()')
+        price_r_s = target.xpath(
+            '//li/div[@class="price"]/p[1]/span[@class="price_r"]/text()')
+        price_s_s = target.xpath(
+            '//li/div[@class="price"]/p[1]/span[@class="price_s"]/text()')
+        price_e_s = target.xpath(
+            '//li/div[@class="price"]/p[2][@class="price_e"]')
+
         with open('./dangdang.csv', 'a', newline='', encoding='gb18030') as f:
             csvwriter = csv.writer(f, delimiter=',')
-            for title, price, link in zip(titles, prices, links):
-                csvwriter.writerow([title, price, link])
+            for name, star, author, publisher, price_n, price_r, price_s in zip(
+                    names, stars, publisher_infos_foreign,
+                    publisher_infos_mainland, price_n_s, price_r_s, price_s_s):
+                csvwriter.writerow(
+                    [name, star, author, publisher, price_n, price_r, price_s])
 
     def main(self):
-        for page in range(1, 11):
-            #url = self.url.format(product, page)
-            html = self.get_html(url)
-            self.parse_html(html)
+        url = self.url
+        html = self.get_html(url)
+        self.parse_html(html)
+        print("完成")
 
 
 if __name__ == '__main__':
